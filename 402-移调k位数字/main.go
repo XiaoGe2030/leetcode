172e2527aa349c2c64eef95ec3961f558f9ce0e4
Liuxiao <1172237608@qq.com>
2021-04-25 08:18:15 +0800

    save code

---
+++ 402-移调k位数字/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	str1 := "21324"
+	str2 := "56778"
+	if str1 > str2 {
+		fmt.Println("str1:", str1)
+		return
+	}
+	fmt.Println("str2:", str2)
+}
+
+func removeKdigits(num string, k int) string {
+
+	return ""
+}
